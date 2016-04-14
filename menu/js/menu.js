8fb916465442cc8a4266af1bdc060fddbeb68b9a
Benjamin Gillissen <benjamin.gillissen@gmail.com>
2016-04-14 10:02:38 +0200

    added a $ in jquery object's var name

--- menu/js/menu.js
+++ menu/js/menu.js
@@ -15,13 +15,14 @@ function themeSwitch(elem){
   	[elem] :	Object, the element that triggered the event
 */
 function themePreview(elem){
+  $prev = $("#preview");
 	if ( typeof(elem) != "object"){ 
-    $("#preview").css("display", "none");
-  	$("#preview").removeProp("src");
+    $prev.css("display", "none");
+  	$prev.removeProp("src");
     return;
   } else if ( $(elem).prop("id").length === 0 ) return;
-  $("#preview").prop("src", "preview/" + $(elem).prop("id") + ".jpg"); 
-  $("#preview").css("display", "block");
+  $prev.prop("src", "preview/" + $(elem).prop("id") + ".jpg"); 
+  $prev.css("display", "block");
 }
 /* 
 	To open with an animation menu entry's childs
@@ -31,7 +32,7 @@ function themePreview(elem){
   	open : 			Boolean, open if true and element's child is closed, close if false
 */
 function menuSlide(elem, childClass, vertical, open){
-	var todo = $(elem).children("." + childClass);
+	var $todo = $(elem).children("." + childClass);
   var attr;
   var animation;
   if ( vertical ){
@@ -40,34 +41,36 @@ function menuSlide(elem, childClass, vertical, open){
     attr = "max-width";		
   }
 	if ( open ){
- 		if ( todo.css("overflow") === "visible"){ return; }
+ 		if ( $todo.css("overflow") === "visible"){ return; }
  		menuZ(elem, true);
-		todo.css(attr, 'none');
+		$todo.css(attr, 'none');
     if ( vertical ){
-  		animation = {maxHeight: todo.height()};
+  		animation = {maxHeight: $todo.height()};
     } else {
-    	animation = {maxWidth: todo.width()};
+    	animation = {maxWidth: $todo.width()};
     	menuPos(elem, childClass);
     }
-  	todo.css(attr, "0px");  
-  	todo.css("display", "block");
-	todo.stop().animate(animation, 
+  	$todo.css(attr, "0px");  
+  	$todo.css("display", "block");
+	$todo.stop().animate(animation, 
 						{duration : 200,
       	                 easing: 'linear',
-        	             complete : function(){ todo.css("overflow", "visible"); } }
+        	             complete : function(){ $todo.css("overflow", "visible"); } }
           	           );
   } else {
-	  todo.css("overflow", "hidden");
+	  $todo.css("overflow", "hidden");
     if ( vertical ){
   		animation = {maxHeight: "0px"}; 
     } else {
     	animation = {maxWidth: "0px"};
     }
-		todo.stop().animate(animation, 
+		$todo.stop().animate(animation, 
 						 	{duration : 200,
                          	 easing: 'linear',
-  							 complete : function(){ todo.css("display", "none");
-                         							menuZ(elem, false); } 
+  							 complete : function(){ 
+                                        $todo.css("display", "none");
+                         				        menuZ(elem, false); 
+                                      }  
                         }
                         );
   }
@@ -79,10 +82,10 @@ function menuSlide(elem, childClass, vertical, open){
 */
 function menuPos(elem, childClass){
 	var ok = false;
-	var father = $(elem).parent();
-	while( $(father).prop('tagName').toLowerCase() != 'nav' ){
-  		if ( $(father).hasClass("dropup") ){ ok = true;break; }
-      father = $(father).parent();
+	var $father = $(elem).parent();
+	while( $father.prop('tagName').toLowerCase() != 'nav' ){
+  		if ( $father.hasClass("dropup") ){ ok = true;break; }
+      $father = $father.parent();
   }
   if ( !ok ) return;
   var brothers = $(elem).parent().children("li");
@@ -100,11 +103,11 @@ function menuPos(elem, childClass){
   	open : Boolean, set zindex to one if true and to zero if false
 */
 function menuZ(elem, open){
-	var nav = $(elem).parent();
-	while( $(nav).prop('tagName').toLowerCase() != 'nav' ){ nav = $(nav).parent(); }
+	var $nav = $(elem).parent();
+	while( $nav.prop('tagName').toLowerCase() != 'nav' ){ $nav = $nav.parent(); }
   if (open){
-  	nav.css("z-index", 1);
+  	$nav.css("z-index", 1);
   } else {
-  	nav.css("z-index", 0);
+  	$nav.css("z-index", 0);
   }
 }
